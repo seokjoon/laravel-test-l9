c540751ee973b0d82820805b57559cca30d6ac1d
seokjoon <seokjoon@gmail.com>
2018-09-25 21:08:56 +0900

    react test init

--- resources/views/t2.blade.php
+++ resources/views/t2.blade.php
@@ -1,2 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+		<script src="{{ asset('js/app.js') }}" defer></script> <!-- DEFER PLEASE -->
+		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	</head>
 
-{{ __FILE__ }}
+	<body>
+		<div id="example">
+		</div>
+	</body>
+</html>
